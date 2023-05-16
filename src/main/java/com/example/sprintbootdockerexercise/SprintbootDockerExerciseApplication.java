b905875546893349b297e60b022b0b010c469b3a
Jason Park <qwt0304@gmail.com>
2023-05-16 14:18:06 +0900

    feat : project build

---
+++ src/main/java/com/example/sprintbootdockerexercise/SprintbootDockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootDockerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootDockerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprintbootdockerexercise/SprintbootDockerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootDockerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
